941d241afbbf748043ca505aa731b7bf0cfcd4bb
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2021-10-04 16:47:26 +0200

    Adding movieCredit Reducer

---
+++ src/redux/reducers/movieCredit.ts
@@ -0,0 +1,43 @@
+import { MovieCreditPayload } from './../Model/index';
+import {
+    GET_MOVIE_CREDITS,
+    GET_MOVIE_CREDITS_SUCCESS,
+    GET_MOVIES_CREDITS_FAILS
+} from './../actionTypes/index';
+
+const initialState = {
+    credits: [],
+    fetching: false,
+}
+
+export const credits = (state = initialState, action: MovieCreditPayload) => {
+    switch (action.type) {
+        case GET_MOVIE_CREDITS: {
+            return {
+                ...state,
+                credits: [],
+                fetching: true,
+            }
+        }
+        case GET_MOVIE_CREDITS_SUCCESS: {
+            return {
+                ...state,
+                fetching: false,
+                credits: action.payload?.cast
+            }
+        }
+        case GET_MOVIES_CREDITS_FAILS: {
+            return {
+                ...state,
+                fetching: false,
+            }
+        }
+
+        default:
+            return state
+    }
+}
+
+
+
+export default credits 
