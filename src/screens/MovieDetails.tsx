c5c01f1b4b44ea9807febbb26d4feda7acf7345c
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2021-10-04 16:49:03 +0200

    Adding movieDetails screen

---
+++ src/screens/MovieDetails.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from 'react'
+import { ScrollView } from 'react-native'
+
+import { useRoute } from '@react-navigation/native'
+import { perfectHeight, perfectWidth } from '../helpers/commonFunctions'
+import styled from 'styled-components/native'
+import Genres from '../components/Genres'
+import { useDispatch, useSelector } from 'react-redux'
+import { getMovieCredits } from '../redux/actions/moviesActions'
+import CastMember from '../components/CastMember'
+import { RouteProp } from '@react-navigation/native';
+import { RootState } from '../redux/Model'
+
+type ParamList = {
+  MovieDetails: {
+    title: string,
+    uri: string,
+    rating: string,
+    overview: string,
+    genres: Array,
+    id: string,
+  };
+};
+
+const MovieDetails = () => {
+
+  const { params: { title, uri, rating, overview, genres, id } } = useRoute<RouteProp<ParamList, 'MovieDetails'>>()
+  const dispatch = useDispatch()
+  const credits = useSelector<RootState>(state => state.credits.credits)
+
+  useEffect(() => {
+    dispatch(getMovieCredits(id))
+  }, [])
+
+  return (
+    <ScrollView contentContainerStyle={{ marginHorizontal: perfectWidth(20) }}>
+      <UpperContainer>
+        <Image source={{ uri: 'https://image.tmdb.org/t/p/original/' + uri }} />
+        <Title>{title}</Title>
+        <Rating>{rating}%</Rating>
+      </UpperContainer>
+      <Title>Overview</Title>
+      <Description>
+        {overview}
+      </Description>
+      <Title>Genres</Title>
+      <Row>
+        {
+          genres?.map((name: string, index: number) => <Genres key={index} text={name} />)
+        }
+      </Row>
+      <Title>Credits</Title>
+      <ScrollView contentContainerStyle={{ marginBottom: perfectHeight(10) }} showsHorizontalScrollIndicator={false} horizontal={true}>
+        {credits?.map(({ name, profile_path }: { name: string, profile_path: string }, index: number) =>
+          <CastMember key={index} name={name}
+            uri={'https://image.tmdb.org/t/p/original/' + profile_path}
+          />
+        )}
+      </ScrollView>
+
+
+    </ScrollView>
+  )
+}
+
+export default MovieDetails
+
+
+const UpperContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-top: ${perfectHeight(50)}px;
+`
+const Image = styled.Image`
+  width:${perfectHeight(140)}px;
+  aspect-ratio: 0.7;
+  margin-bottom: ${perfectHeight(15)}px;
+`
+
+const Title = styled.Text`
+  font-weight: bold;
+  font-size: 23px;
+  margin-bottom: ${perfectHeight(10)}px;
+`
+const Rating = styled.Text`
+  color: #73F340;
+  font-size: 30px;
+  font-weight: bold;
+`
+const Description = styled.Text`
+  font-size: 15px;
+  margin-bottom:${perfectHeight(10)}px
+`
+const Row = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom:${perfectHeight(10)}px
+
+`
+
