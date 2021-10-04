b1017337400da60ba5c75a7012de5f3e67816a7c
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2021-10-04 16:47:38 +0200

    Adding models for defining types for redux

---
+++ src/redux/Model/index.ts
@@ -0,0 +1,55 @@
+
+export interface MoviesReducer {
+    movies: Object[],
+    fetching: boolean,
+    hasMore: boolean,
+    params: {
+        page: number,
+    },
+}
+export interface GenresReducer {
+    genres: {
+        [id: string]: Object
+    },
+    fetching: boolean,
+}
+
+export interface MovieCreditReducer {
+    credits: {
+        credits: Object[],
+        fetching: boolean,
+    }
+}
+
+export interface GenresPayLoad {
+    payload: { genres: { id: string, name: string }[] }, type: string
+}
+
+export interface MovieCreditPayload {
+    payload: {
+        cast: Object[],
+    },
+    type: string
+}
+
+export interface MoviesPayload {
+    payload:
+    {
+        results: Object[],
+        total_pages: number,
+        params: {
+            page: number,
+        }
+    }
+    ,
+    type: string
+}
+
+export interface RootState {
+    popularMovies: MoviesReducer
+    upcomingMovies: MoviesReducer
+    topRatedMovies: MoviesReducer
+    genres: GenresReducer
+    credits: MovieCreditReducer
+
+}
